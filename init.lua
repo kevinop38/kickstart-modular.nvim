c0b9fe86c24ba2b5f7e17cb6c8432029f5250873
kevinop38 <kevinop38@gmail.com>
2025-11-22 19:46:18 +0800

    change config

--- init.lua
+++ init.lua
@@ -19,15 +19,16 @@ require 'lazy-plugins'
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
-vim.g.clipboard = {
-  name = 'WslClipboard',
-  copy = {
-    ['+'] = 'clip.exe',
-    ['*'] = 'clip.exe',
-  },
-  paste = {
-    ['+'] = 'powershell.exe Get-Clipboard',
-    ['*'] = 'powershell.exe Get-Clipboard',
-  },
-  cache_enabled = 0,
-}
+-- vim.g.clipboard = {
+--   name = 'WslClipboard',
+--   copy = {
+--     ['+'] = 'clip.exe',
+--     ['*'] = 'clip.exe',
+--   },
+--   paste = {
+--     ['+'] = 'powershell.exe Get-Clipboard',
+--     ['*'] = 'powershell.exe Get-Clipboard',
+--   },
+--   cache_enabled = 0,
+-- }
+-- vim.opt.fileformats = { 'unix', 'dos' }
